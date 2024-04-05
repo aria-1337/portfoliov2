ede235bec313644095e1108ffc0e9dce5bf5a373
aria-1337 <aria.lopez.dev@gmail.com>
2024-04-05 00:17:13 -0700

    Fade in on page load

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import styled, { keyframes } from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [landed, setLanded] = useState<boolean>(false);
+    useEffect(() => {
+        setLanded(true);
+    }, []);
+    useEffect(() => {
+        setTimeout(() => {
+            setLanded(false);
+        }, 2000);
+    }, [landed]);
+    return (<Container>
+            { landed ? 
+            <Landing>
+                <LandingTitle>Aria Lopez</LandingTitle>
+                <LandingSubTitle>Fullstack Engineer</LandingSubTitle>
+            </Landing> : null }
+        </Container>); 
 }
 
-export default App;
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    width: 100vw;
+    align-items: center;
+    justify-content: center;
+`;
+
+const rotate = keyframes`
+    0% { opacity: 1; }
+    100% { opacity: 0; }
+`;
+
+const Landing = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    animation: ${rotate} 2s linear;
+`;
+
+
+const LandingTitle = styled.h1`
+    margin: 0;
+`;
+
+const LandingSubTitle = styled.h3`
+    margin: 0;
+    color: gray;
+`;
