7b9d7d021c868e53d46ac701dca81680d08e0436
aria-1337 <aria.lopez.dev@gmail.com>
2024-04-05 17:57:34 -0700

    Updated stars effect

--- src/components/Effect.jsx
+++ src/components/Effect.jsx
@@ -11,7 +11,8 @@ import { Canvas } from '@react-three/fiber';
 function Meshes(props) {
     return (
         <group {...props} dispose={null}>
-            <Sparkles count={5000} scale={75} size={10} speed={1} opacity={0.3} color={'white'} />
+            <Sparkles count={10000} scale={73} size={10} speed={0.5} opacity={0.2} color={'white'} />
+            <Sparkles count={10000} scale={73} size={10} speed={0.5} opacity={0.2} color={'orange'} />
         </group>
     );
 }
@@ -21,9 +22,9 @@ export default function Effect(props) {
         <Canvas camera={{ near: 0.1, position: [0, 0, 25], fov: '65'}}>
             <OrbitControls 
                 makeDefault
-                dollySpeed={0.1}
-                maxDistance={45}
-                minDistance={45}
+                dollySpeed={1}
+                maxDistance={100}
+                minDistance={100}
                 target={[2, -2, 0]}
             />
             <ambientLight intensity={0.5} />
